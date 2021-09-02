a508f09a4b249c8cc6506f3dd9fd5852b74e6987
Emil Kovacev <emilkova@buffalo.edu>
2021-09-02 00:52:52 -0400

    fixed errors and debugged first objective code until completion

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,27 @@
 import socketserver
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
+    @classmethod
+    def find_path(self, request):
+        return request.split(' ')[1]
+
     def handle(self):
-        recieved_data = self.request.recv(1024).strip()
-        print(self.client_address[0] + ' is sending data:')
-        print(recieved_data.decode())
-        print('\n\n')
-        response =  'HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello world!'.encode()
-        self.request.sendall(response)
+        recieved_data = self.request.recv(1024).strip() # recieve data
+        path = self.find_path(recieved_data.decode())   # find host from decoded str
+
+        if path == '/hello':
+            response =  """HTTP/1.1 200 OK\r\n
+                        Content-Length: 12\r\n\r\n
+                        Hello world!""".encode()        # encode response body
+            self.request.sendall(response)              # send response
+
+        else:
+            response =  """HTTP/1.1 404 Not Found\r\n
+                        Content-Type: text/plain\r\n
+                        content-length: 36\r\n\r\n
+                        The requested content does not exist""".encode()
+
+            self.request.sendall(response)
 
 
 if __name__ == '__main__':
