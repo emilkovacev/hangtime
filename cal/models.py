3b0f3fb514ee90a968d50d4effc3818f2dc2ebad
adam <adam38999@gmail.com>
2021-10-14 16:36:30 -0400

    added models.py, included start_time, end_time, duration, name, description

--- cal/models.py
+++ cal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Event(models.Model):
+    start_time = models.DateTimeField
+    end_time = models.DateTimeField
+    duration = models.DurationField
+    name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=200)
+
+
 # Create your models here.
