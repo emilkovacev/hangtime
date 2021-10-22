02aad94af2115b9f79c3edbcc073bc77e0178915
Emil Kovacev <emilkova@buffalo.edu>
2021-10-22 01:42:54 -0400

    added form submission overlay

--- cal/templates/cal/script.js
+++ cal/templates/cal/script.js
@@ -1,20 +1,27 @@
+// GLOBAL variables
 var HEIGHT = document.body.offsetHeight;
+var SHIFT = 40;
 var CalendarEvent = /** @class */ (function () {
-    function CalendarEvent(name, start_time, end_time, color) {
+    function CalendarEvent(name, start_time, end_time) {
         this.name = name;
         this.start_time = start_time;
         this.end_time = end_time;
-        this.color = color;
+        this.color = '#949494';
+        this.description = '';
     }
     return CalendarEvent;
 }());
 var parseTime = function (date) { return date.getHours() * 60 + date.getMinutes(); };
+// this list should be sorted by date when populated!
+// sorting the list will help event layering work properly
 var events_list = [
-    new CalendarEvent('Event A', new Date('October 20, 2021 3:00'), new Date('October 20, 2021 5:00'), 'green'),
-    new CalendarEvent('Event B', new Date('October 20, 2021 13:00'), new Date('October 20, 2021 18:00'), 'red'),
-    new CalendarEvent('Event Z', new Date('October 20, 2021 13:00'), new Date('October 20, 2021 18:00'), 'purple'),
-    new CalendarEvent('Event C', new Date('October 20, 2021 15:00'), new Date('October 20, 2021 18:00'), 'blue'),
-    new CalendarEvent('Event A', new Date('October 20, 2021 23:00'), new Date('October 20, 2021 23:59'), 'orange'),
+    new CalendarEvent('Event D', new Date('October 20, 2021 0:00'), new Date('October 20, 2021 23:59')),
+    new CalendarEvent('Event D', new Date('October 20, 2021 2:15'), new Date('October 20, 2021 22:00')),
+    new CalendarEvent('Event A', new Date('October 20, 2021 3:00'), new Date('October 20, 2021 5:00')),
+    new CalendarEvent('Event B', new Date('October 20, 2021 13:00'), new Date('October 20, 2021 18:00')),
+    new CalendarEvent('Event Z', new Date('October 20, 2021 13:00'), new Date('October 20, 2021 18:00')),
+    new CalendarEvent('Event C', new Date('October 20, 2021 15:00'), new Date('October 20, 2021 18:00')),
+    new CalendarEvent('Event A', new Date('October 20, 2021 23:00'), new Date('October 20, 2021 23:59')),
 ];
 function intersections(event) {
     var count = 0;
@@ -35,6 +42,12 @@ function intersections(event) {
     }
     return count;
 }
+function loadTimes() {
+    var calendar = document.getElementById('calendar');
+    for (var i = 0; i <= 24; i++) {
+        calendar;
+    }
+}
 function loadEvents() {
     var calendar = document.getElementById('calendar');
     var event;
@@ -43,17 +56,34 @@ function loadEvents() {
         var eventBlock = document.createElement("div");
         eventBlock.innerHTML = "<div class=\"content\">\n                            <b>" + event.name + "</b>\n                            <p>" + event.start_time.toLocaleTimeString() + " - " + event.end_time.toLocaleTimeString() + "</p>\n                            </div>";
         eventBlock.className = 'event';
-        var start = parseTime(event.start_time);
-        var end = parseTime(event.end_time);
+        var start = Math.round(parseTime(event.start_time)) + calendar.offsetTop + 20;
+        var end = parseTime(event.end_time) + calendar.offsetTop + 20;
         console.log('start: ' + event.start_time.getHours() + ', end: ' + event.end_time.getHours());
         eventBlock.style.top = (Math.round(start)).toString() + 'px';
         eventBlock.style.height = (Math.round(end - start)).toString() + 'px';
         eventBlock.style.backgroundColor = event.color;
-        eventBlock.style.marginLeft = (40 * intersections(event)).toString() + 'px';
+        eventBlock.style.marginLeft = (SHIFT * intersections(event)).toString() + 'px';
         calendar.appendChild(eventBlock);
     }
 }
 function load() {
+    loadTimes();
     loadEvents();
 }
+function toggleEventForm() {
+    var form = document.getElementById('form');
+    var calendar = document.getElementById('calendar');
+    var new_button = document.getElementById('new');
+    if (form.style.display === 'flex') {
+        form.style.display = 'none';
+        calendar.style.display = 'block';
+        new_button.innerHTML = '+';
+    }
+    else {
+        form.style.display = 'flex';
+        calendar.style.display = 'none';
+        new_button.innerHTML = 'X';
+    }
+}
 document.addEventListener('DOMContentLoaded', load);
+document.getElementById('new').addEventListener('click', toggleEventForm);
--- cal/templates/cal/script.ts
+++ cal/templates/cal/script.ts
@@ -1,4 +1,6 @@
+// GLOBAL variables
 const HEIGHT = document.body.offsetHeight
+const SHIFT = 40;
 
 
 class CalendarEvent {
@@ -6,24 +8,31 @@ class CalendarEvent {
   start_time: Date;
   name: string;
   color: string;
+  description: string;
 
-  constructor(name: string, start_time: Date, end_time: Date, color: string) {
+  constructor(name: string, start_time: Date, end_time: Date) {
     this.name = name;
     this.start_time = start_time;
     this.end_time = end_time;
-    this.color = color;
+    this.color = '#949494';
+    this.description = '';
   }
 }
 
 const parseTime = (date: Date) => date.getHours() * 60 + date.getMinutes();
 
 
+// this list should be sorted by date when populated!
+// sorting the list will help event layering work properly
+
 let events_list: CalendarEvent[] = [
-  new CalendarEvent('Event A', new Date('October 20, 2021 3:00'), new Date('October 20, 2021 5:00'), 'green'),
-  new CalendarEvent('Event B', new Date('October 20, 2021 13:00'), new Date('October 20, 2021 18:00'), 'red'),
-  new CalendarEvent('Event Z', new Date('October 20, 2021 13:00'), new Date('October 20, 2021 18:00'), 'purple'),
-  new CalendarEvent('Event C', new Date('October 20, 2021 15:00'), new Date('October 20, 2021 18:00'), 'blue'),
-  new CalendarEvent('Event A', new Date('October 20, 2021 23:00'), new Date('October 20, 2021 23:59'), 'orange'),
+  new CalendarEvent('Event D', new Date('October 20, 2021 0:00'), new Date('October 20, 2021 23:59')),
+  new CalendarEvent('Event D', new Date('October 20, 2021 2:15'), new Date('October 20, 2021 22:00')),
+  new CalendarEvent('Event A', new Date('October 20, 2021 3:00'), new Date('October 20, 2021 5:00')),
+  new CalendarEvent('Event B', new Date('October 20, 2021 13:00'), new Date('October 20, 2021 18:00')),
+  new CalendarEvent('Event Z', new Date('October 20, 2021 13:00'), new Date('October 20, 2021 18:00')),
+  new CalendarEvent('Event C', new Date('October 20, 2021 15:00'), new Date('October 20, 2021 18:00')),
+  new CalendarEvent('Event A', new Date('October 20, 2021 23:00'), new Date('October 20, 2021 23:59')),
 ];
 
 function intersections(event: CalendarEvent): number {
@@ -36,6 +45,13 @@ function intersections(event: CalendarEvent): number {
     else if (cal_event.start_time <= event.end_time && event.end_time <= cal_event.end_time) {count += 1;}
   }
   return count;
+}
+
+function loadTimes(): void {
+  const calendar = document.getElementById('calendar');
+  for (let i=0; i<=24; i++) {
+    calendar
+  }
 }
 
 function loadEvents(): void {
@@ -51,22 +67,39 @@ function loadEvents(): void {
 
     eventBlock.className = 'event';
 
-    const start: number = parseTime(event.start_time);
-    const end: number = parseTime(event.end_time);
+    const start: number = Math.round(parseTime(event.start_time)) + calendar.offsetTop + 20;
+    const end: number = parseTime(event.end_time) + calendar.offsetTop + 20;
 
     console.log('start: ' + event.start_time.getHours() + ', end: ' + event.end_time.getHours())
 
     eventBlock.style.top = (Math.round(start)).toString() + 'px';
     eventBlock.style.height = (Math.round(end-start)).toString() + 'px';
     eventBlock.style.backgroundColor = event.color;
-    eventBlock.style.marginLeft = (40*intersections(event)).toString() + 'px';
+    eventBlock.style.marginLeft = (SHIFT*intersections(event)).toString() + 'px';
 
     calendar.appendChild(eventBlock);
   }
 }
 
 function load() {
+  loadTimes();
   loadEvents();
 }
 
+function toggleEventForm() {
+  const form = document.getElementById('form');
+  const calendar = document.getElementById('calendar');
+  const new_button = document.getElementById('new');
+  if (form.style.display === 'flex') {
+    form.style.display = 'none';
+    calendar.style.display = 'block';
+    new_button.innerHTML = '+';
+  } else {
+    form.style.display = 'flex';
+    calendar.style.display = 'none';
+    new_button.innerHTML = 'X';
+  }
+}
+
 document.addEventListener('DOMContentLoaded', load);
+document.getElementById('new').addEventListener('click', toggleEventForm)
