c28216ce225d2b4125cf2ece404ffde12852b1f5
Emil Kovacev <emilkova@buffalo.edu>
2021-10-31 03:20:19 -0400

    Completed objective 3
    - fixed position of reloaded elements
    - catches bad file descriptor error (if socket is closed)

--- views/chat.py
+++ views/chat.py
@@ -6,14 +6,16 @@ import json
 
 
 def load_prev_messages(socket):
-    for client in socket.clients:
-        for data in messages.get_messages():
-            message: str = json.dumps({'username': data['username'], 'comment': data['comment']})
-            message_frame = Frame(
-                FIN=1, RSV1=0, RSV2=0, RSV3=0,
-                opcode=1, payload_len=len(message), MASK=0, data=message.encode()
-            )
-            client.request.sendall(message_frame.write_raw())
+    for data in messages.get_messages():
+        message: str = json.dumps({'username': data['username'], 'comment': data['comment']})
+        message_frame = Frame(
+            FIN=1, RSV1=0, RSV2=0, RSV3=0,
+            opcode=1, payload_len=len(message), MASK=0, data=message.encode()
+        )
+        try:
+            socket.request.sendall(message_frame.write_raw())
+        except OSError:
+            pass
 
 
 def websocket(socket, request: Request) -> None:
@@ -34,7 +36,10 @@ def websocket(socket, request: Request) -> None:
             messages.create_message(data['username'], data['comment'])
 
             for client in socket.clients:
-                client.request.sendall(frame.write_raw())
+                try:
+                    client.request.sendall(frame.write_raw())
+                except OSError:
+                    pass
 
     else:
         raise InvalidRequest
