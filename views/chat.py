bceaeafe839a1d59b6f965076b59999e645a8527
adam3 <adam38999@gmail.com>
2021-12-10 13:48:29 -0500

    finished chat

--- views/chat.py
+++ views/chat.py
@@ -1,3 +1,5 @@
+import bcrypt
+
 from crablib.fileIO import FileIO
 from crablib.http.parse import Request, parse_frame, Frame
 from crablib.http.response import http_200, handshake_response, InvalidRequest
@@ -5,6 +7,8 @@ from crablib.auth import get_request_account
 from db import messages
 from db import account as acc
 import json
+
+from db.account import get_account_from_token
 
 
 def index(socket, request: Request):
@@ -43,24 +47,32 @@ def load_message(socket):
         if frame.opcode == 8: break                                 # end websocket connection
 
         data = json.loads(frame.data)
-        messages.create_message(username=data['username'], comment=data['comment'])  # store message in db
+        #messages.create_message(username=data['username'], comment=data['comment'])  # store message in db
 
-        for client in socket.clients:                               # send message to each connected client
-            try:
-                client.request.sendall(frame.write_raw())
-            except OSError:
-                pass
+        if data['username'] in socket.chatclients:
+            for client in socket.chatclients[data['username']]:                               # send message to each connected client
+                try:
+                    client.request.sendall(frame.write_raw())
+                except OSError:
+                    pass
 
 
 def websocket(socket, request: Request) -> None:
     if request.request_type == 'GET':
         # implement websocket handshake
-        socket.clients.append(socket)
+        auth_token = request.cookies['auth_token']
+        auth_token_salt = b'$2b$12$Fr9yR03IQLCGqjB1MJ9gfu'
+        auth_token_hash = bcrypt.hashpw(auth_token.encode(), auth_token_salt).decode()
+        username = get_account_from_token(auth_token_hash)["username"]
+        if username in socket.chatclients:
+            socket.chatclients[username].append(socket)
+        else:
+            socket.chatclients[username] = [socket]
         key = request.headers['Sec-WebSocket-Key']
         response = handshake_response(key).write_raw()
         socket.request.sendall(response)
 
-        load_prev_messages(socket)  # load messages sent before user loaded
+        #load_prev_messages(socket)  # load messages sent before user loaded
         load_message(socket)        # load any messages sent while websocket is open
 
     else:
